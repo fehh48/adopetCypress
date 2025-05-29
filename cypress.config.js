07f4ea5bd1afb9fc4783f019ccef6a6ef960ae0a
fehh48 <felipe4813@live.com>
2025-05-29 18:31:49 +1000

    Implementando CI e mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,
+    reporter: 'mochawesome', 
+    reporterOptions: {
+      reportDir: 'cypress/results', // pasta onde vai ficar os reportes
+      overwrite: false,  //// nao vai subreescrever encima dos nossos testes
+      html: true,      //pode gerar arquivo em HTML. pode gerar em mais de um framework
+      json: false,
+      timestamp: "mmddyyyy_HHMMss" }   // formato de hora dia e ETc, podendo ser mudado quando quiser
   },
 });
