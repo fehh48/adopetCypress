674c2c85a8e0f7002bf2b855405fcd4758a460a4
fehh48 <felipe4813@live.com>
2025-05-30 17:54:05 +1000

    int de API em mensagens

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,11 @@
 describe('Api Adopet', () =>{
-  const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlMWMzZGM4Mi0zYTExLTQ0Y2YtODA1NC1iZWU0ODRlYmM3YWIiLCJhZG9wdGVyTmFtZSI6IkZlbGlwZSBCIFNpbHZhIiwiaWF0IjoxNzQ4NTg4NjgyLCJleHAiOjE3NDg4NDc4ODJ9.1UAffqdZTAfSVrJRrXM-Ipa6YvmRp3KhXr78_NwZDvc`
+  const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlMWMzZGM4Mi0zYTExLTQ0Y2YtODA1NC1iZWU0ODRlYmM3YWIiLCJhZG9wdGVyTmFtZSI6IkZlbGlwZSBCIFNpbHZhIiwiaWF0IjoxNzQ4NTg4ODkyLCJleHAiOjE3NDg4NDgwOTJ9.taLVy9lQcBcx3RqBLdv__Kb1iOF6FZ0hAABTlrRyRTs`
 
   it('Mensagens da API', () =>{
     cy.request({
     method: 'GET' ,
     url: 'https://adopet-api-i8qu.onrender.com/mensagem/e1c3dc82-3a11-44cf-8054-bee484ebc7ab',
-    Headers: { authorization }
+    headers: { authorization }
   }).then((res) => {
         expect(res.status).to.be.equal(200)
         expect(res.body).is.not.empty
